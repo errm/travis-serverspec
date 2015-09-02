de8f61b668d0f2c606fbe81c545a17d281d2a836
Ed Robinson <ed.robinson@reevoo.com>
2015-09-02 15:42:59 +0100

    Don’t need to install curl

--- start_docker.sh
+++ start_docker.sh
@@ -14,11 +14,6 @@ DOCKER_PORT_RANGE=2400:2500
 SLIRP_PORTS=$(seq 2000 2500)
 export HOST_IP DOCKER_HOST DOCKER_PORT_RANGE SLIRP_PORTS
 
-echo 'Installing cURL'
-sudo apt-get -y update
-sudo apt-get install -y ca-certificates curl
-echo ''
-
 echo 'Installing docker repository'
 curl https://get.docker.com/gpg | sudo apt-key add -
 echo 'deb https://get.docker.io/ubuntu docker main' | sudo tee /etc/apt/sources.list.d/docker.list
